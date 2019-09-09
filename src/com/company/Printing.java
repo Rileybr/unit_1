74cd853af1ae58bd9236592d58bc7f1e0766980c
rileybr <brandon.riley@ssfs.org>
2019-09-09 14:11:57 -0400

    Part 1 of exercise 1.1

---
+++ src/com/company/Printing.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Printing {
+
+    public static void main(String[] args) {
+        System.out.println("14:09");
+        System.out.println("09/09/19");
+        System.out.println("It is a great \ntime to be a \ncomputer programmer!");
+    }
+}
