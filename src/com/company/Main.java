29c52c9de85b6aa8b5efbec27b1a861d9010be3f
rileybr <brandon.riley@ssfs.org>
2019-09-09 14:07:29 -0400

    Added a third print statement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Hello World");
-	    System.out.println("What's Good?");
+	    System.out.println("Howdy?");
+        System.out.println("\"Brandon\"");
     }
 }
