1166fe20c5be03d5fe3336a99e916778047e5dc1
rileybr <brandon.riley@ssfs.org>
2019-09-10 14:39:41 -0400

    Part 1 of exercise 1.1 prints time made, date, and a phrase broken up into three lines

--- src/com/company/Printing.java
+++ src/com/company/Printing.java
@@ -6,5 +6,8 @@ public class Printing {
         System.out.println("14:09");
         System.out.println("09/09/19");
         System.out.println("It is a great \ntime to be a \ncomputer programmer!");
+        System.out.print("It is a great");
+        System.out.print(" time to be a");
+        System.out.print(" computer programmer!");
     }
 }
