9f9bf8a66b8ce16a252196e8d2917b8a0e2bd63d
rileybr <brandon.riley@ssfs.org>
2019-09-13 10:12:36 -0400

    calculates the number of gallons of paint required to paint in a whole number a room based on room size given by the user

--- src/com/company/Painting.java
+++ src/com/company/Painting.java
@@ -1,10 +1,20 @@
 package com.company;
+import java.util.Scanner;
+
+
 
 public class Painting {
     public static void main(String [] args){
         double length;
         double width;
         double height;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter room length in feet:");
+        length = scan.nextDouble();
+        System.out.println("Enter room width in feet:");
+        width = scan.nextDouble();
+        System.out.println("Enter room height in feet:");
+        height = scan.nextDouble();
         int door_number;
         int window_number;
         int door_area = 20;
@@ -12,9 +22,6 @@ public class Painting {
         double room_surface_area;
         final int paint;
         double paint_needed;
-        length = 32;
-        width = 40;
-        height = 16;
         door_number = 2;
         window_number = 4;
         room_surface_area = length*width + 2*length*height + 2*width*height - door_area*door_number - window_area*window_number;
@@ -24,7 +31,7 @@ public class Painting {
         paint = 350;
         paint_needed = room_surface_area/paint;
         System.out.print("you will need ");
-        System.out.print(paint_needed);
+        System.out.print(Math.round(paint_needed));
         System.out.println(" gallons of paint. :)");
 
 
