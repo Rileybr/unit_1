faae8978b65efcac2527320cb951189b6e544f4a
rileybr <brandon.riley@ssfs.org>
2019-09-16 10:43:09 -0400

    calculates the area of a rectangle with side lengths of 6 and 14 and then prints the area

---
+++ src/com/company/RectangleArea.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class RectangleArea {
+    public static void main(String [] args){
+        int width = 6;
+        int length = 14;
+        int area;
+        area = width * length;
+        System.out.println("Area = " + area);
+    }
+}
