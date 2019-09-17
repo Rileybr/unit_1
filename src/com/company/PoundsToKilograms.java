eafa3bb6192f7621f9e2c38cc51625b65bc9c040
rileybr <brandon.riley@ssfs.org>
2019-09-17 12:30:45 -0400

    converts user input weight in lbs to kg

--- src/com/company/PoundsToKilograms.java
+++ src/com/company/PoundsToKilograms.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class PoundsToKilograms {
     public static void main(String[] args){
-        double lbs = 115;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter weight in lbs:");
+        double lbs = scan.nextDouble();
         double kg;
         kg = lbs / 2.205;
         System.out.println(lbs + " pounds = " + kg + " kilograms");
