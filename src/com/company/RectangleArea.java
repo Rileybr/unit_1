74d3fdbf48748e484ae5cc700bbe67bf2c533439
rileybr <brandon.riley@ssfs.org>
2019-09-17 12:32:24 -0400

    calculates area of a rectangle after getting width and length from user

--- src/com/company/RectangleArea.java
+++ src/com/company/RectangleArea.java
@@ -1,9 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class RectangleArea {
     public static void main(String [] args){
-        int width = 6;
-        int length = 14;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter width:");
+        int width = scan.nextInt();
+        System.out.println("Enter length:");
+        int length = scan.nextInt();
         int area;
         area = width * length;
         System.out.println("Area = " + area);
