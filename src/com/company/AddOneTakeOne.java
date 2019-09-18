de5615cdce32e5215f467b225f618f9e6db093e3
rileybr <brandon.riley@ssfs.org>
2019-09-18 13:46:15 -0400

    user inputs a number, and it prints the number, the number beneath it and the number above it in numerical order

---
+++ src/com/company/AddOneTakeOne.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class AddOneTakeOne {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number:  ");
+        int number = scan.nextInt();
+
+        number--;
+        System.out.print(number);
+        number++;
+        System.out.print(number);
+        number++;
+        System.out.print(number);
+    }
+}
